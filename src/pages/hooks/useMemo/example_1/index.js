a2f2eb4107a3ef3365c2124082b363dc965dce88
saifur rahman <saifuroracle@gmail.com>
2022-08-09 19:56:41 +0600

    layout

--- src/pages/hooks/useMemo/example_1/index.js
+++ src/pages/hooks/useMemo/example_1/index.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import {Card, Button} from 'react-bootstrap';
 
-function UseMemoExample1() {
+function UseMemoHookExample1() {
   return (
     <Card>
       <Card.Header as="h5">useMemo hook Example 1</Card.Header>
       <Card.Body>
         <Card.Title>useMemo hook</Card.Title>
         <Card.Text>
-          
+
         </Card.Text>
       </Card.Body>
     </Card>
   )
 }
 
-export default UseMemoExample1
+export default UseMemoHookExample1
