ba408f34a1a73ff465fe829ec36369c89e63fd01
saifur rahman <saifuroracle@gmail.com>
2022-08-09 20:34:48 +0600

    UseMemoHookExample1

--- src/pages/hooks/useMemo/example_1/index.js
+++ src/pages/hooks/useMemo/example_1/index.js
@@ -1,14 +1,31 @@
-import React from 'react'
-import {Card, Button} from 'react-bootstrap';
+import React, {useState} from 'react'
+import { useMemo } from 'react';
+import {Card, Button, Form, InputGroup} from 'react-bootstrap';
 
 function UseMemoHookExample1() {
+
+  const [users, setUsers] = useState([
+    {'id': '1', 'name': 'Anwar'},
+    {'id': '2', 'name': 'Brad'},
+    {'id': '3', 'name': 'Carl'},
+    {'id': '4', 'name': 'Dexter'},
+    {'id': '5', 'name': 'Esper'},
+    {'id': '6', 'name': 'Fred'},
+  ]);
+
+  const selectedUser = useMemo(() => {
+    return users.find(itm=>itm?.id==6)
+  }, [users])
+ 
   return (
     <Card>
       <Card.Header as="h5">useMemo hook Example 1</Card.Header>
       <Card.Body>
         <Card.Title>useMemo hook</Card.Title>
         <Card.Text>
-
+            {
+                selectedUser?.name
+            }
         </Card.Text>
       </Card.Body>
     </Card>
